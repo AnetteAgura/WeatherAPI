cea270b116b5d1566bb4adfc4b5487a19efad145
AnetteAguraiuja <anetteagura@gmail.com>
2022-01-27 21:20:07 +0200

    Working BUT userPosition won't update, so no centering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
 import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
-import React, { useEffect, useState } from "react";
+import React, {Props, useEffect, useState} from "react";
 import {LeafletMouseEvent} from "leaflet";
 import {DomUtil, latLng, Util} from 'leaflet';
 import { url } from 'inspector';
 import { isCatchClause } from 'typescript';
 import { render } from '@testing-library/react';
-
-type WeatherData = {
-  list: [];
-}
 
 type Coordinate = {
     latitude: number,
@@ -18,37 +14,39 @@ type Coordinate = {
 
 export default function App(){
 
-    const [weatherData, setWeatherData] = useState<WeatherData>();
+    const [weatherData, setWeatherData] : any = useState([]);
     const [userPosition, setUserPosition] = useState<Coordinate>({latitude: 58.3780, longitude: 26.7290});
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
+            const {latitude, longitude} = position.coords;
             setUserPosition({latitude, longitude});
+
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=38cd7ebc4d661b9be1f9bde599415364&units=metric`)
+                .then(res => res.json())
+                .then(data => {
+                    setWeatherData(data)
+                    console.log(data);
+                }).catch(console.error)
         }, () => {
-            alert("Must share location for accurate weather information")
+            alert("You must allow location service to see accurate weather information.")
         })
     },  []);
 
-    const GetWeather = () => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${userPosition.latitude}&lon=${userPosition.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-            .then(res => res.json())
-            .then(result => {
-                setWeatherData(result)
-                console.log(result);
-            });
-    }
-
     const Markers = () => {
 
-            useMapEvents({
-                click(e: LeafletMouseEvent) {
-                    setUserPosition({
-                        latitude: e.latlng.lat,
-                        longitude: e.latlng.lng
-                    });
+        useMapEvents({
+            click(e: LeafletMouseEvent) {
+                let lat = e.latlng.lat
+                let lng = e.latlng.lng
+                fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=38cd7ebc4d661b9be1f9bde599415364&units=metric`)
+                    .then(res => res.json())
+                    .then(data => {
+                        setWeatherData(data)
+                        console.log(data);
+                    }). catch(console.error)
                 },
-            });
+        });
         return (userPosition ?
                 <Marker
                     key={1}
@@ -58,22 +56,15 @@ export default function App(){
         )
     }
 
-       return(<MapContainer center={[userPosition.latitude, userPosition.longitude]} zoom={13} scrollWheelZoom={true} id={"map"}>
+    return(<MapContainer center={[userPosition.latitude, userPosition.longitude]} zoom={9} scrollWheelZoom={true} id={"map"}>
 
-           <Marker position={[userPosition.latitude, userPosition.longitude]}/>
+        <Marker position={[userPosition.latitude, userPosition.longitude]}/>
 
-           <Markers />
+        <Markers />
 
-           <TileLayer
-               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-           />
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      </MapContainer>)}
-
-
-
-
-
-
-
+    </MapContainer>)}
