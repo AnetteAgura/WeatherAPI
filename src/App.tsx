501ca7382b19a1d699227a1666fb33d85ca3fb58
AnetteAguraiuja <anetteagura@gmail.com>
2022-01-27 17:32:02 +0200

    Not centering and taking wrong initial coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,75 @@
 import './App.css';
 import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import React, { useEffect, useState } from "react";
+import {LeafletMouseEvent} from "leaflet";
 import {DomUtil, latLng, Util} from 'leaflet';
 import { url } from 'inspector';
 import { isCatchClause } from 'typescript';
 import { render } from '@testing-library/react';
 
-interface IWeatherData {
+type WeatherData = {
   list: [];
 }
 
-interface ILocation {
-    latitude: number;
-    longitude: number;
+type Coordinate = {
+    latitude: number,
+    longitude: number
 }
 
 export default function App(){
 
-    const [weatherData, setWeatherData] = useState<IWeatherData>();
-    const [initialPosition, setInitialPosition] = useState<[number, number]>([58.3780, 26.7290]);
+    const [weatherData, setWeatherData] = useState<WeatherData>();
+    const [userPosition, setUserPosition] = useState<Coordinate>({latitude: 58.3780, longitude: 26.7290});
 
-    const Markers = () => {
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            setUserPosition({latitude, longitude});
+        }, () => {
+            alert("Must share location for accurate weather information")
+        })
+    },  []);
 
-      const map = useMapEvents({
-          click(e) {
-              setInitialPosition([
-                  e.latlng.lat,
-                  e.latlng.lng
-
-              ]);
-              GetWeather();
-              },
-        });
-
-      return ( initialPosition ?
-              <Marker
-              key={initialPosition[1]}
-              position={initialPosition}
-              interactive={false}
-              /> : null
-      )}
     const GetWeather = () => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${initialPosition[0]}&lon=${initialPosition[1]}&APPID=a874d3d875a1b67b607feaa88782161d&units=metric`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${userPosition.latitude}&lon=${userPosition.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
             .then(res => res.json())
             .then(result => {
                 setWeatherData(result)
                 console.log(result);
             });
-
     }
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
-            setInitialPosition([latitude, longitude]);
+    const Markers = () => {
 
-        });
-    }, [initialPosition]);
+            useMapEvents({
+                click(e: LeafletMouseEvent) {
+                    setUserPosition({
+                        latitude: e.latlng.lat,
+                        longitude: e.latlng.lng
+                    });
+                },
+            });
+        return (userPosition ?
+                <Marker
+                    key={1}
+                    position={[userPosition.latitude, userPosition.longitude]}
+                    interactive={true}
+                /> : null
+        )
+    }
 
-       return<MapContainer center={initialPosition} zoom={13} scrollWheelZoom={true} id={"map"}>
+       return(<MapContainer center={[userPosition.latitude, userPosition.longitude]} zoom={13} scrollWheelZoom={true} id={"map"}>
+
+           <Marker position={[userPosition.latitude, userPosition.longitude]}/>
 
            <Markers />
-
-           <Marker position={initialPosition}/>
 
            <TileLayer
                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
            />
 
-      </MapContainer>}
+      </MapContainer>)}
 
 
 
